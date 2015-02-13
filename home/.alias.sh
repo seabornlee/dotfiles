3e2e5a5a966e71d6baf812ae0b3d068b004d4f31
Seaborn Lee <seabornlee.cn@gmail.com>
2015-02-13 10:50:44 +0800

    add alias to edit alias file

--- home/.alias.sh
+++ home/.alias.sh
@@ -1,4 +1,5 @@
 alias e='vim'
+alias ealias='vim ~/.alias.sh'
 alias tmux="TERM=screen-256color-bce tmux"
 
 alias gst='git status'
@@ -6,3 +7,6 @@ alias gfxd='git clean -fxd'
 alias bi='bundle install'
 
 alias rort='RAILS_ENV=test bundle exec'
+
+alias nig='npm install -g'
+alias bis='bower install -S'
