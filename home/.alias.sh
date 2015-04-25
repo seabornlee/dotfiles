e4c47755522eb724cd2628bed12bb1af312dbfab
Seaborn Lee <seabornlee.cn@gmail.com>
2015-04-26 07:55:40 +0800

    Install the fuck: https://github.com/nvbn/thefuck

--- home/.alias.sh
+++ home/.alias.sh
@@ -10,3 +10,10 @@ alias rort='RAILS_ENV=test bundle exec'
 
 alias nig='npm install -g'
 alias bis='bower install -S'
+
+alias yoad='yo angular:directive '
+
+# the fuck: https://github.com/nvbn/thefuck
+alias fuck='eval $(thefuck $(fc -ln -1))'
+# You can use whatever you want as an alias, like for Mondays:
+alias FUCK='fuck'
